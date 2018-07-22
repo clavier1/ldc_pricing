0015f7dae151e2faff533709aaa5b8f14e25eba5
clavier1 <>
2018-07-22 17:06:55 -0400

    build fat jar, remove async call to read json file

--- ldc_pricing/src/main/java/ldc/vertx/LDCService.java
+++ ldc_pricing/src/main/java/ldc/vertx/LDCService.java
@@ -1,14 +1,19 @@
 package ldc.vertx;
 
+import java.io.File;
+import java.util.Map;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 
 public class LDCService {
     private static final Logger logger = LogManager.getLogger(LDCService.class);
-    private static final String BTC_SUB_CONFIG_FILE = "resources/btc_sub.json";
+    private static final String BTC_SUB_CONFIG_FILE = "src/main/resources/btc_sub.json";
     
     public static void main(String[] argv) {
         Vertx vertx = Vertx.vertx();
@@ -28,15 +33,16 @@ public class LDCService {
     }
     
     private static JsonObject createSubConfig(Vertx vertx) {
-        JsonObject subConfig = new JsonObject();
+        JsonObject subConfig = null;
         
-        vertx.fileSystem().readFile(BTC_SUB_CONFIG_FILE, result -> {
-           if (result.succeeded()) {
-               subConfig.mergeIn(new JsonObject(result.result()));
-           } else {
-               logger.fatal("Failed to read subscription file: {}", BTC_SUB_CONFIG_FILE, result.cause());
-           }
-        });
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, Object> objectMap = mapper.readValue(new File(BTC_SUB_CONFIG_FILE), Map.class);
+            subConfig = new JsonObject(objectMap);
+            
+        } catch (Exception e) {
+            logger.error("Failed to read subscription file {} to json object", BTC_SUB_CONFIG_FILE, e);
+        }
         
         return subConfig;
     }
