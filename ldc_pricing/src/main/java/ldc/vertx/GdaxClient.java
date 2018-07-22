7e173ca0ec54423b902a066b05acaa146f4eb1b0
clavier1 <>
2018-07-22 01:43:03 -0400

    make gdax subscription configurable

--- ldc_pricing/src/main/java/ldc/vertx/GdaxClient.java
+++ ldc_pricing/src/main/java/ldc/vertx/GdaxClient.java
@@ -1,8 +1,5 @@
 package ldc.vertx;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,47 +8,34 @@ import io.vertx.core.buffer.Buffer;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.RequestOptions;
-import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import ldc.utils.GdaxMessageType;
 
 public class GdaxClient extends AbstractVerticle {
     private static final Logger logger = LogManager.getLogger(GdaxClient.class);
-
-    private static final String BTC_USD = "BTC-USD";
+    
+    private final JsonObject subConfig;
+    
+    public GdaxClient(JsonObject subConfig) {
+        this.subConfig = subConfig;
+    } 
     
     @Override
     public void start() throws Exception {
         RequestOptions ro = new RequestOptions();
         ro.setHost("ws-feed.gdax.com").setPort(443).setSsl(true).setURI("/");
         
-        JsonObject subscription = createSubscription();
-        
         HttpClient client = vertx.createHttpClient();
         client.websocket(ro, websocket -> {
             websocket.handler(data -> {
-                handleData(data);              
-//                String info = data.toString("ISO-8859-1");
-
+                
+                handleData(data);
+                
             });
             
-            websocket.writeTextMessage(subscription.toString());
+            websocket.writeTextMessage(subConfig.toString());
         });
-    
         
-    }
-    
-    private JsonObject createSubscription() {
-        // a bit hard coded. should be some xml configurable if multiple subscriptions. 
-        JsonArray currency = new JsonArray(new ArrayList<String>(Arrays.asList("BTC-USD")));
-        JsonArray field = new JsonArray(new ArrayList<String>(Arrays.asList("matches")));
-        
-        JsonObject subscription = new JsonObject();
-        subscription.put("type", "subscribe");
-        subscription.put("product_ids", currency);
-        subscription.put("channels", field);
-        
-        return subscription;
     }
     
     private void handleData(Buffer data) {
--- ldc_pricing/src/main/java/ldc/vertx/LDCService.java
+++ ldc_pricing/src/main/java/ldc/vertx/LDCService.java
@@ -4,15 +4,40 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 
 public class LDCService {
     private static final Logger logger = LogManager.getLogger(LDCService.class);
+    private static final String BTC_SUB_CONFIG_FILE = "resources/btc_sub.json";
     
     public static void main(String[] argv) {
         Vertx vertx = Vertx.vertx();
 
         logger.info("Starting {} and {}", GdaxTradeServer.class.getSimpleName(), GdaxClient.class.getSimpleName());
+        
+        JsonObject subConfig = createSubConfig(vertx);
+        if (subConfig.isEmpty()) {
+            // fail fast
+            logger.fatal("No subscription config found for GdaxClient. Exit!");
+            System.exit(1);
+        }
+        
+        GdaxClient gdaxClient = new GdaxClient(subConfig);
+        vertx.deployVerticle(gdaxClient);
         vertx.deployVerticle(GdaxTradeServer.class.getName());
-        vertx.deployVerticle(GdaxClient.class.getName());
+    }
+    
+    private static JsonObject createSubConfig(Vertx vertx) {
+        JsonObject subConfig = new JsonObject();
+        
+        vertx.fileSystem().readFile(BTC_SUB_CONFIG_FILE, result -> {
+           if (result.succeeded()) {
+               subConfig.mergeIn(new JsonObject(result.result()));
+           } else {
+               logger.fatal("Failed to read subscription file: {}", BTC_SUB_CONFIG_FILE, result.cause());
+           }
+        });
+        
+        return subConfig;
     }
 }
